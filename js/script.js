f2cd0146f3684e356c824cc2f033f7a1a8046e51
Matia <matia.casu@gmail.com>
2021-04-07 18:04:06 +0200

    Aggiunti lista visibile in "blocco" e funzione alternativa ad arrow

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@
 Attraverso l'apposita API di Boolean
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
-
 var app = new Vue({
   el:"#app",
   data: {
-    emailList: []
+    emailList: [],
+    listLength: 10
   },
   mounted: function(){
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < this.listLength; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((response) => {
         this.emailList.push(response.data.response);
       });
     }
-  }
+  },
+
+  // Alternativa alla arrow function
+  // mounted: function(){
+  //   const self = this;
+  //   for(var i = 0; i < this.listLength; i++) {
+  //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //     .then(function (response){
+  //       self.emailList.push(response.data.response);
+  //     });
+  //   }
+  // },
+
 });
