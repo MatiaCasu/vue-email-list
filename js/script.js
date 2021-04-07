5aad7e4b682a252fe764763c97a4fa3c41eae995
Matia <matia.casu@gmail.com>
2021-04-07 17:06:48 +0200

    Stampata a schermo lista con 10 email differenti

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 var app = new Vue({
   el:"#app",
   data: {
-    email: ""
+    emailList: []
   },
   mounted: function(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response) => {
-    this.email = response.data.response;
-    });
+    for(var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((response) => {
+        this.emailList.push(response.data.response);
+      });
+    }
   }
 });
